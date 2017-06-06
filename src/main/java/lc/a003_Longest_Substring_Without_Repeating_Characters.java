9d39545af21c462fd054dec28e9c9d8c950c44fc
Jasonexp <jasonexp@live.com>
2017-06-05 20:05:44 -0700

    Update

--- src/main/java/lc/a003_Longest_Substring_Without_Repeating_Characters.java
+++ src/main/java/lc/a003_Longest_Substring_Without_Repeating_Characters.java
@@ -8,6 +8,18 @@ import java.util.Set;
 /**
  * Created by zguo on 3/29/17.
  * *
+ Difficulty: Medium
+ Contributor: LeetCode
+
+ Given a string, find the length of the longest substring without repeating characters.
+
+ Examples:
+
+ Given "abcabcbb", the answer is "abc", which the length is 3.
+
+ Given "bbbbb", the answer is "b", with the length of 1.
+
+ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 public class a003_Longest_Substring_Without_Repeating_Characters {
     public int lengthOfLongestSubstring(String s) {
--- src/main/java/lc/a004_Median_of_Two_Sorted_Arrays.java
+++ src/main/java/lc/a004_Median_of_Two_Sorted_Arrays.java
@@ -3,6 +3,22 @@ package lc;
 /**
  * Created by zguo on 3/29/17.
  * *
+ Difficulty: Hard
+ Contributor: LeetCode
+ There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+ Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+ Example 1:
+ nums1 = [1, 3]
+ nums2 = [2]
+
+ The median is 2.0
+ Example 2:
+ nums1 = [1, 2]
+ nums2 = [3, 4]
+
+ The median is (2 + 3)/2 = 2.5
  */
 public class a004_Median_of_Two_Sorted_Arrays {
 
